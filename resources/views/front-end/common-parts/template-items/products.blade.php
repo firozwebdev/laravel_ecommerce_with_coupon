4e11b2eb7c2d77f2a458594279288410c9f57aea
sabuzwebdev <frsmgt@gmail.com>
2018-03-07 14:08:55 +0600

    Small amendment on route

--- resources/views/front-end/common-parts/template-items/products.blade.php
+++ resources/views/front-end/common-parts/template-items/products.blade.php
@@ -24,7 +24,7 @@
                                 <ul class="list-inline">
                                     <li><a href="cart.html" data-toggle="tooltip" data-placement="top" title="Cart"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
                                     <li><a href="my-wishlist.html" data-toggle="tooltip" data-placement="top" title="Wishlist"><i class="fa fa-heart-o" aria-hidden="true"></i></a></li>
-                                    <li><a href="{{ route('shop.show',[ 'product' => $product->slug ]) }}" data-toggle="tooltip" data-placement="top" title="Quickview"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+                                    <li><a href="{{ route('store.show',[ 'product' => $product->slug ]) }}" data-toggle="tooltip" data-placement="top" title="Quickview"><i class="fa fa-search" aria-hidden="true"></i></a></li>
                                 </ul>
                             </div>
                         </div>
